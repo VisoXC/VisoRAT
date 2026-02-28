c100d59a89b5f7aee4d30dda95acf819e584133c
VisoXD <153297821+VisoXC@users.noreply.github.com>
2026-02-28 14:54:07 +0100

    removed debug flag

--- server/server.py
+++ server/server.py
@@ -271,4 +271,5 @@ if __name__ == '__main__':
         print("  Username validation: Via player head API")
     
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
